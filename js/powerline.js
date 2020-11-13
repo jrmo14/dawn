7c915426280948ec77975239e3efa54714dfa15c
Jackson <jrmoffet@gmail.com>
2020-11-13 02:07:25 -0500

    Add clicky pages

--- js/powerline.js
+++ js/powerline.js
@@ -4,6 +4,19 @@ class Powerline {
 
     for (let i = 1; i <= panels; $('.indicator').innerHTML += `<li ${i++}></li>`)
 
+    $('.indicator').onclick = (e) => {
+      e = e || window.event;
+      var target = e.target || e.srcElement;
+      if(target.attributes['length'] === 0) {
+        this.activate('#tabs ul li', `#tabs ul li:nth-child(1)`);
+        this.activate('#panels > ul', `#panels ul:nth-child(1)`);
+      } else if (target.attributes['0'].nodeName != 'active') {
+        let idx = parseInt(target.attributes['0'].nodeName) + 1;
+        this.activate('#tabs ul li', `#tabs ul li:nth-child(${idx})`);
+        this.activate('#panels > ul', `#panels ul:nth-child(${idx})`);
+      }
+    }
+
     document.onkeydown = (e) => {
       if (e != undefined) {
         let key = e.key;
